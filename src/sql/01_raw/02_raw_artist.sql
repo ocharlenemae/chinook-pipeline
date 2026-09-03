87225f88b3870ba91037f8cb4dd1bea1ca31d997
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 09:58:04 +0800

    Update 02_raw_artist.sql

--- src/sql/01_raw/02_raw_artist.sql
+++ src/sql/01_raw/02_raw_artist.sql
@@ -1,5 +1,5 @@
 -- ARTIST TABLE --
-CREATE TABLE IF NOT EXISTS chinook.chinook_artist AS
+CREATE TABLE IF NOT EXISTS chinook.chinook_raw.artist AS
 SELECT *
 FROM read_files(
     '/Volumes/ftw/chinook/ftw-b12-de/shared/week05/chinook_csv/Artist.csv',
