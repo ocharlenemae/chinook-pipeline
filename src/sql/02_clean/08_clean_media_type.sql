745af9bf44e8c1c6020465d9bf836de009fe80e7
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:26:50 +0800

    Update 08_clean_media_type.sql

--- src/sql/02_clean/08_clean_media_type.sql
+++ src/sql/02_clean/08_clean_media_type.sql
@@ -1,1 +1,8 @@
-
+-- MEDIA TYPE TABLE --
+CREATE OR REPLACE TABLE IF EXISTS chinook.chinook_clean.media_type AS
+SELECT 
+    -- Cleaned media format lookup table
+    CAST(MediaTypeId AS INT) AS media_type_id,     -- Primary key
+    TRIM(Name) AS media_type_name
+FROM chinook.chinook_raw.media_type
+WHERE MediaTypeId IS NOT NULL;
