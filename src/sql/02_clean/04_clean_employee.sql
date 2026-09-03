ae81d77e2676a3fd011962f72b4cc3fd6ae1ef9c
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:18:45 +0800

    Update 04_clean_employee.sql

--- src/sql/02_clean/04_clean_employee.sql
+++ src/sql/02_clean/04_clean_employee.sql
@@ -1,1 +1,20 @@
-
+-- EMPLOYEE TABLE --
+CREATE OR REPLACE TABLE IF NOT EXISTS chinook.chinook_clean.employee AS
+SELECT 
+    -- Cleaned employee directory with formatted dates, addresses, and phone numbers
+    CAST(EmployeeId AS INT) AS employee_id,     -- Primary key
+    CONCAT(TRIM(FirstName), ' ', TRIM(LastName)) AS employee_name,
+    TRIM(Title) AS title,
+    CAST(ReportsTo AS INT) AS reports_to,     -- Self-referencing foreign key -> employee_clean
+    CAST(BirthDate AS DATE) AS birth_date,
+    CAST(HireDate AS DATE) AS hire_date,
+    REGEXP_REPLACE(Address, '[.,]', '') AS employee_address,
+    TRIM(City) AS employee_city,
+    TRIM(State) AS employee_state,
+    TRIM(Country) AS employee_country,
+    UPPER(TRIM(PostalCode)) AS employee_postal_code,
+    CONCAT('+', REGEXP_REPLACE(Phone, '[^0-9]', '')) AS employee_phone,
+    CONCAT('+', REGEXP_REPLACE(Fax, '[^0-9]', '')) AS employee_fax,
+    TRIM(Email) AS employee_email
+FROM chinook.chinook_raw.employee
+WHERE EmployeeId IS NOT NULL;
