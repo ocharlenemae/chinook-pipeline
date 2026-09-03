6d755c23ee0a2c9749029de3fb7b587241608835
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 09:58:59 +0800

    Update 04_raw_employee.sql

--- src/sql/01_raw/04_raw_employee.sql
+++ src/sql/01_raw/04_raw_employee.sql
@@ -1,1 +1,9 @@
-
+-- EMPLOYEE TABLE --
+CREATE TABLE IF NOT EXISTS chinook.chinook_raw.employee AS
+SELECT *
+FROM read_files(
+    '/Volumes/ftw/chinook/ftw-b12-de/shared/week05/chinook_csv/Employee.csv',
+    format => 'csv',
+    header => True,
+    schema => 'EmployeeId INT, LastName STRING, FirstName STRING, Title STRING, ReportsTo INT, BirthDate DATE, HireDate DATE, Address STRING, City STRING, State STRING, Country STRING, PostalCode STRING, Phone STRING, Fax STRING, Email STRING'
+);
