48d5888db280c009e297bcc09651001436c20a43
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:01:49 +0800

    Update 08_raw_media_type.sql

--- src/sql/01_raw/08_raw_media_type.sql
+++ src/sql/01_raw/08_raw_media_type.sql
@@ -1,1 +1,9 @@
-
+-- MEDIA TYPE TABLE --
+CREATE TABLE IF NOT EXISTS chinook.chinook_raw.media_type AS
+SELECT *
+FROM read_files(
+    '/Volumes/ftw/chinook/ftw-b12-de/shared/week05/chinook_csv/MediaType.csv',
+    format => 'csv',
+    header => True,
+    schema => 'MediaTypeId INT, Name STRING'
+);
