fc779f2a7c258224d5c3960f2b5e233bf9874d89
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:03:10 +0800

    Update 10_raw_playlist_track.sql

--- src/sql/01_raw/10_raw_playlist_track.sql
+++ src/sql/01_raw/10_raw_playlist_track.sql
@@ -1,1 +1,9 @@
-
+-- PLAYLIST TRACK TABLE --
+CREATE TABLE IF NOT EXISTS chinook.chinook_raw.playlist_track AS
+SELECT *
+FROM read_files(
+    '/Volumes/ftw/chinook/ftw-b12-de/shared/week05/chinook_csv/PlaylistTrack.csv',
+    format => 'csv',
+    header => True,
+    schema => 'PlaylistId INT, TrackId INT'
+);
