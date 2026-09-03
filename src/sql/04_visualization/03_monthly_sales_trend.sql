d9873870771dcfebe30aac1e4b2c5366203af1e8
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:50:13 +0800

    Update 03_monthly_sales_trend.sql

--- src/sql/04_visualization/03_monthly_sales_trend.sql
+++ src/sql/04_visualization/03_monthly_sales_trend.sql
@@ -1,6 +1,6 @@
 --  MONTHLY SALES TREND --
 -- Tracks monthly total invoice revenue over the 2-year window (Jan 2024 - Dec 2025). --
-CREATE OR REPLACE TABLE chinook.chinook_visualization.monthly_sales_trend_mv AS
+CREATE OR REPLACE TABLE chinook.chinook_visualization.monthly_sales_trend AS
 SELECT
   d.month_and_year AS date,
   ROUND(SUM(f.total_revenue), 2) AS total_revenue
