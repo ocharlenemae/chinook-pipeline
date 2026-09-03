d4e4529de8bbf5a9ec9f1541bdb8ef1c08e85e73
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:30:16 +0800

    Update 09_clean_playlist.sql

--- src/sql/02_clean/09_clean_playlist.sql
+++ src/sql/02_clean/09_clean_playlist.sql
@@ -1,1 +1,8 @@
-
+-- PLAYLIST TABLE --
+CREATE OR REPLACE TABLE chinook.chinook_clean.playlist AS
+SELECT 
+    -- Cleaned playlist table with trimmed titles
+    CAST(PlaylistId AS INT) AS playlist_id,     -- Primary key
+    TRIM(Name) AS playlist_name
+FROM chinook.chinook_raw.playlist
+WHERE PlaylistId IS NOT NULL;
