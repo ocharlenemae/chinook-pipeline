621459b2e82d4a614169b953a4dcfbec008d6ae0
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 09:56:34 +0800

    Update 03_raw_customer.sql

--- src/sql/01_raw/03_raw_customer.sql
+++ src/sql/01_raw/03_raw_customer.sql
@@ -1,1 +1,9 @@
-
+-- CUSTOMER TABLE --
+CREATE TABLE IF NOT EXISTS chinook.chinook_customer AS
+SELECT *
+FROM read_files(
+    '/Volumes/ftw/chinook/ftw-b12-de/shared/week05/chinook_csv/Customer.csv',
+    format => 'csv',
+    header => True,
+    schema => 'CustomerId INT, FirstName STRING, LastName STRING, Company STRING, Address STRING, City STRING, State STRING, Country STRING, PostalCode STRING, Phone STRING, Fax STRING, Email STRING, SupportRepId INT'
+);
