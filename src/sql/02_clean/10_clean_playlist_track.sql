ff4fd20b399011d13c746d747757c5bca6f23cfa
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:31:06 +0800

    Update 10_clean_playlist_track.sql

--- src/sql/02_clean/10_clean_playlist_track.sql
+++ src/sql/02_clean/10_clean_playlist_track.sql
@@ -1,1 +1,8 @@
-
+-- PLAYLIST TRACK TABLE --
+CREATE OR REPLACE TABLE chinook.chinook_clean.playlist_track AS
+SELECT
+    -- Cleaned table mapping playlists to tracks 
+    CAST(PlaylistId AS INT) AS playlist_id,    -- Foreign key -> playlist_clean
+    CAST(TrackId AS INT) AS track_id     -- Foreign key -> track_clean
+FROM chinook.chinook_raw.playlist_track
+WHERE PlaylistId IS NOT NULL AND TrackId IS NOT NULL;
