4a4d7d1d9dd5254be846b58586cd4de2793b3b34
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:03:51 +0800

    Update 11_raw_track.sql

--- src/sql/01_raw/11_raw_track.sql
+++ src/sql/01_raw/11_raw_track.sql
@@ -1,1 +1,9 @@
-
+-- TRACK TABLE --
+CREATE TABLE IF NOT EXISTS chinook.chinook_raw.track AS
+SELECT *
+FROM read_files(
+    '/Volumes/ftw/chinook/ftw-b12-de/shared/week05/chinook_csv/Track.csv',
+    format => 'csv',
+    header => True,
+    schema => 'TrackId INT, Name STRING, AlbumId INT, MediaTypeId INT, GenreId INT, Composer STRING, Milliseconds INT, Bytes BIGINT, UnitPrice DOUBLE'
+);
