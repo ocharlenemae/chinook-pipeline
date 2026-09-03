abff94c3311c9e87071d1aac81382ff5f0738035
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:19:53 +0800

    Update 05_clean_genre.sql

--- src/sql/02_clean/05_clean_genre.sql
+++ src/sql/02_clean/05_clean_genre.sql
@@ -1,1 +1,8 @@
-
+-- GENRE TABLE --
+CREATE OR REPLACE TABLE IF NOT EXISTS chinook.chinook_clean.genre AS
+SELECT 
+    -- Cleaned genre table with trimmed genre names
+    CAST(GenreId AS INT) AS genre_id,     -- Primary key
+    TRIM(Name) AS genre_name
+FROM chinook.chinook_raw.genre
+WHERE GenreId IS NOT NULL;
