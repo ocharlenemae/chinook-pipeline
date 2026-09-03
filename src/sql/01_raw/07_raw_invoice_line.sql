a68e9e6f8e67c6a488458de62f0c6e654d44f423
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:01:09 +0800

    Update 07_raw_invoice_line.sql

--- src/sql/01_raw/07_raw_invoice_line.sql
+++ src/sql/01_raw/07_raw_invoice_line.sql
@@ -1,1 +1,9 @@
-
+-- INVOICE LINE TABLE --
+CREATE TABLE IF NOT EXISTS chinook.chinook_raw.invoice_line AS
+SELECT *
+FROM read_files(
+    '/Volumes/ftw/chinook/ftw-b12-de/shared/week05/chinook_csv/InvoiceLine.csv',
+    format => 'csv',
+    header => True,
+    schema => 'InvoiceLineId INT, InvoiceId INT, TrackId INT, UnitPrice DOUBLE, Quantity INT'
+);
