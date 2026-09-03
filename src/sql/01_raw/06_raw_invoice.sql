320161bd8d8e5e395fc3514fac96bf0284b1924e
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:00:26 +0800

    Update 06_raw_invoice.sql

--- src/sql/01_raw/06_raw_invoice.sql
+++ src/sql/01_raw/06_raw_invoice.sql
@@ -1,1 +1,9 @@
-
+-- INVOICE TABLE --
+CREATE TABLE IF NOT EXISTS chinook.chinook_raw.invoice AS
+SELECT *
+FROM read_files(
+    '/Volumes/ftw/chinook/ftw-b12-de/shared/week05/chinook_csv/Invoice.csv',
+    format => 'csv',
+    header => True,
+    schema => 'InvoiceId INT, CustomerId INT, InvoiceDate TIMESTAMP, BillingAddress STRING, BillingCity STRING, BillingState STRING, BillingCountry STRING, BillingPostalCode STRING, Total DOUBLE'
+);
