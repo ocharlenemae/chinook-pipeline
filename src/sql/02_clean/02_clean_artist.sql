1e8fd4a7f754e0c1bc38501f994a4da658cb13d0
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:10:47 +0800

    Update 02_clean_artist.sql

--- src/sql/02_clean/02_clean_artist.sql
+++ src/sql/02_clean/02_clean_artist.sql
@@ -1,1 +1,8 @@
-
+-- ARTIST TABLE --
+CREATE OR REPLACE TABLE chinook.chinook_clean.artist AS
+SELECT 
+    -- Cleaned artist table with trimmed names
+    CAST(ArtistId AS INT) AS artist_id,     -- Primary key 
+    TRIM(Name) AS artist_name     
+FROM chinook.chinook_raw.artist
+WHERE ArtistId IS NOT NULL;
