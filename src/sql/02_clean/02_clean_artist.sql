562f2d0eed92b17e9db5c8560d8e2617e9a661d9
hazelle.cuevas@ftwfoundation.org <hazelle.cuevas@ftwfoundation.org>
2026-09-03 07:40:23 +0000

    update artist name (NULL  --> unknown)

--- src/sql/02_clean/02_clean_artist.sql
+++ src/sql/02_clean/02_clean_artist.sql
@@ -1,8 +1,8 @@
 -- ARTIST TABLE --
 CREATE OR REPLACE TABLE chinook.chinook_clean.artist AS
 SELECT 
-    -- Cleaned artist table with trimmed names
-    CAST(ArtistId AS INT) AS artist_id,     -- Primary key 
-    TRIM(Name) AS artist_name     
+    -- Cleaned artist table with trimmed names and unknown artist names
+    CAST(ArtistId AS INT) AS artist_id,     -- Primary key
+    COALESCE(TRIM(Name), 'Unknown') AS artist_name
 FROM chinook.chinook_raw.artist
 WHERE ArtistId IS NOT NULL;
