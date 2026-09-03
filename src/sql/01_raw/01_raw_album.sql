537e83dace13671c5830ef71060a6965b0bf55e9
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 09:54:20 +0800

    Update 01_raw_album.sql

--- src/sql/01_raw/01_raw_album.sql
+++ src/sql/01_raw/01_raw_album.sql
@@ -1,1 +1,9 @@
-
+-- ALBUM TABLE --
+CREATE TABLE IF NOT EXISTS chinook.chinook_album AS
+SELECT *
+FROM read_files(
+    '/Volumes/ftw/chinook/ftw-b12-de/shared/week05/chinook_csv/Album.csv',
+    format => 'csv',
+    header => True,
+    schema => 'AlbumId INT, Title STRING, ArtistId INT'
+);
