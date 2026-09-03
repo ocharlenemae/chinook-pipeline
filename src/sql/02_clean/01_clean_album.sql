8fd045fcd52121a9ff58fad5e2d8085aee2e5413
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:06:22 +0800

    Update 01_clean_album.sql

--- src/sql/02_clean/01_clean_album.sql
+++ src/sql/02_clean/01_clean_album.sql
@@ -1,1 +1,9 @@
-
+-- ALBUM TABLE --
+CREATE OR REPLACE TABLE chinook.chinook_clean.album AS
+SELECT
+    -- Clean the album table with trimmed strings and enforced data types
+    CAST(AlbumId AS INT) AS album_id,     -- Primary key
+    TRIM(Title) AS album_title,     
+    CAST(ArtistId AS INT) AS artist_id     -- Foreign key -> artist_clean
+FROM chinook.chinook_raw.album
+WHERE AlbumId IS NOT NULL;
