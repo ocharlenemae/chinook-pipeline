81a24b52278f1a527baa9283a5f1e411b4f5cd1a
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 10:36:29 +0800

    Update 02_dim_date.sql

--- src/sql/03_mart/02_dim_date.sql
+++ src/sql/03_mart/02_dim_date.sql
@@ -1,1 +1,8 @@
-
+-- DATE DIMENSION --
+CREATE OR REPLACE TABLE chinook.chinook_mart.dim_date AS
+SELECT DISTINCT
+  INT(DATE_FORMAT(invoice_date, 'yyyyMMdd')) AS date_key,
+  invoice_date AS date,
+  DATE_FORMAT(invoice_date, 'MMM yyyy') AS month_and_year,
+  DATE_FORMAT(invoice_date, 'yyyy-\QQ') AS year_and_quarter
+FROM chinook.chinook_clean.invoice;
