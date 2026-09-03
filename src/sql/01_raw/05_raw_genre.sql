26e8a4fb6db0e2592ad91868f328af3f09abb2e6
ocharlenemae <74508014+ocharlenemae@users.noreply.github.com>
2026-09-03 09:59:43 +0800

    Update 05_raw_genre.sql

--- src/sql/01_raw/05_raw_genre.sql
+++ src/sql/01_raw/05_raw_genre.sql
@@ -1,1 +1,9 @@
-
+-- GENRE TABLE --
+CREATE TABLE IF NOT EXISTS chinook.chinook_raw.genre AS
+SELECT *
+FROM read_files(
+    '/Volumes/ftw/chinook/ftw-b12-de/shared/week05/chinook_csv/Genre.csv',
+    format => 'csv',
+    header => True,
+    schema => 'GenreId INT, Name STRING'
+);
