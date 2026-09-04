d0a44d07eee576c00623957390da80c29c716e98
orogocharlenemae@gmail.com <orogocharlenemae@gmail.com>
2026-09-04 03:43:31 +0000

    Fix syntax error in dim_track JOIN clause

--- src/sql/03_mart/05_fact_invoice_line.sql
+++ src/sql/03_mart/05_fact_invoice_line.sql
@@ -24,7 +24,7 @@ SELECT
 FROM chinook.chinook_clean.invoice_line AS il
 INNER JOIN chinook.chinook_clean.invoice AS i
   ON il.invoice_id = i.invoice_id
-LEFT JOIN chinook.chinook_mart.dim_track LIVE.dim_track AS t
+LEFT JOIN chinook.chinook_mart.dim_track AS t
   ON il.track_id = t.track_id
 LEFT JOIN chinook.chinook_mart.dim_customer AS c
   ON i.customer_id = c.customer_id
